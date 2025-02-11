96400a396555befdda3d0d815439aee5333c076b
menaka <127004769+bitgenerater@users.noreply.github.com>
2025-02-11 11:24:21 +0530

    Create user get functions

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { connect } from 'mongoose';
 import connectDB from './config/mongodb.js'
 import authRouter from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js';
 
 
 const app = express();
@@ -19,7 +20,8 @@ app.use(cors({credentials:true}));
 
 app.get('/', (req,res)=> res.send("API working"));
 
-app.use('/api/auth',authRouter )
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 
 
 
